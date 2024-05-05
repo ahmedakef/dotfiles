6753b682197c76b85e42320b36b9036153c2910c
ahmedakef <aakef@uber.com>
2024-05-05 09:45:54 +0200

    remove the dependency on DOTFILES_DIR

--- vim/install.sh
+++ vim/install.sh
@@ -2,18 +2,14 @@
 set -ox pipefail
 
 
-if [ -z "$DOTFILES_DIR" ]; then
-    echo "Error: DOTFILES_DIR environment variable is not set."
-    exit 1
-fi
 
 mkdir -p ~/.vim/bundle
 
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 fi
-ln -s "$DOTFILES_DIR/vim/plugins.vim" ~/.vim/plugins.vim
-ln -s "$DOTFILES_DIR/vim/.vimrc" ~/.vimrc
+ln -s plugins.vim ~/.vim/plugins.vim
+ln -s .vimrc ~/.vimrc
 
 
 if [[ "$(uname)" == "Linux" ]]; then
--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,7 +1,6 @@
 # General
 export LANG="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-export DOTFILES_DIR="$HOME/Personal/dotfiles"
 export PATH=$PATH:$HOME/go/bin/
 export EDITOR="vim"
 alias python=python3
