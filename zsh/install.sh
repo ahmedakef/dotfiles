9b02fb2d3e4007dab1e8b674afce9d703c1f1455
ahmedakef <aakef@uber.com>
2024-05-05 15:21:49 +0200

    update

--- zsh/install.sh
+++ zsh/install.sh
@@ -20,11 +20,12 @@ if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
-brew install pure bat fd
 
 if [[ "$(uname)" == "Linux" ]]; then
     sudo apt-get update
-    sudo apt-get install pure bat fd-find
+    # use this apt for updated environments
+    # but if package manager have old versions, use the following
+    # sudo apt-get install pure bat fd-find
     # installing bat
     wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb
     sudo dpkg -i bat-musl_0.24.0_amd64.deb
@@ -33,6 +34,10 @@ if [[ "$(uname)" == "Linux" ]]; then
     wget https://github.com/junegunn/fzf/releases/download/0.50.0/fzf-0.50.0-linux_amd64.tar.gz
     tar xvf fzf-0.50.0-linux_amd64.tar.gz
     sudo cp fzf /usr/local/bin
+
+    # installing fd
+    wget https://github.com/sharkdp/fd/releases/download/v9.0.0/fd-musl_9.0.0_amd64.deb
+    sudo dpkg -i fd-musl_9.0.0_amd64.deb
 else
     brew install pure bat fd
 fi
