cd0e0249b600224e6e99e90cbbfd8e0cd85e2812
ahmedakef <aakef@uber.com>
2024-05-05 09:46:10 +0200

    add rtmux alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -52,6 +52,15 @@ tj() {
 }
 
 
+# usage rtmux hostname usage name
+rtmux() {
+        case "$2" in
+            "") autossh -M 0 $1 -t "if tmux -qu has; then tmux -qu attach; else EDITOR=emacs tmux -qu new; fi";;
+            *) autossh -M 0 $1 -t "if tmux -qu has -t $2; then tmux -qu attach -t $2; else EDITOR=emacs tmux -qu new -s $2; fi";;
+        esac
+    }
+
+
 
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ ! -e /usr/local/bin/fd ]]; then
